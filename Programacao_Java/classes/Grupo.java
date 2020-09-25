0f120d9e6c05e3454187f59a9eb5697adffc400d
Felipe Magrinelli <60147664+Femagrinelli@users.noreply.github.com>
2020-09-25 19:56:22 -0300

    Update Grupo.java

--- Programacao_Java/classes/Grupo.java
+++ Programacao_Java/classes/Grupo.java
@@ -19,27 +19,27 @@ public class Grupo {
 
     private Chat chatGrupo = new Chat("Grupo", 1);
 
-    public Grupo(int códigoGrupo, String nomeGrupo, String descriçãoGrupo, Professor professor){
+    public Grupo(int códigoGrupo, String nomeGrupo, String descriçãoGrupo, Professor professorAdmin){
         this.códigoGrupo = new Código(códigoGrupo, "Grupo");
         this.nomeGrupo = nomeGrupo;
         this.descriçãoGrupo = descriçãoGrupo;
-        this.professorAdmin = professor;
+        this.professorAdmin = professorAdmin;
     }
 
     public void adicionarProfessor(Professor professor){
         listaParticipantes.add(professor);
     }    
 
-    public void escolherModeradores(){
+    public void escolherModeradores(Professor novoModerador){
         for(Professor participante : listaParticipantes){
-            if(participante.equals("Eduardo")){
+            if(participante.equals(novoModerador)){
                 listaModeradores.add(participante);
             }    
         }
     }
 
     public void criarPostagemGrupo(){
-        Postagem postagem = new Postagem(7, "Leões", "Esse grupo só tem fera");
+        Postagem postagem = new Postagem(7, "Leões", "Esse grupo só tem fera");//EXEMPLO
         postagensGrupo.add(postagem);
     }
 
